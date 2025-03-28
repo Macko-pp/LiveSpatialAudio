7bd06d17f9b7947a45c227ba66bdc93a00c94018
Macko++ <82177845+Macko-pp@users.noreply.github.com>
2025-03-27 22:50:14 -0500

    3D Audio on a different page from homemade stereo. Everything  centered

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-	import '../app.css';
+	import "../app.css";
+	import { Button } from "$lib/components/ui/button";
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="w-100% mb-3 h-15 bg-slate-100 p-2 flex justify-center space-x-2">
+	<Button class="bg-slate-300 font-bold" href="/">Homemade Stereo</Button>
+	<Button class="bg-slate-300 font-bold" href="/3dAudio">Dynamic 3D Audio</Button>
+</div>
+
+<div class="flex justify-center">
+	<div>{@render children()}</div>
+</div>
